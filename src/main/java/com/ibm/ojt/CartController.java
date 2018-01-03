38cf640254c9190faca0095fdc75d72a080f486b
thereserayza <thereserayzaaaaaa@gmail.com>
2018-01-03 19:05:54 +0800

    Updating CartController and logic

--- src/main/java/com/ibm/ojt/CartController.java
+++ src/main/java/com/ibm/ojt/CartController.java
@@ -1,6 +1,11 @@
 package com.ibm.ojt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,14 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cart/{cartId}")
+@RequestMapping("/cart")
 public class CartController {
 	
 	@Autowired
-	CartRepository cartRepository;
+	MongoTemplate mongoTemplate;
+	
+//	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+//	public void addToCart(@RequestBody Cart cart) {
+//		cartRepository.save(cart);
+//	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void addToCart(@RequestBody Cart cart) {
-		cartRepository.save(cart);
+		Query query = new Query();
+		query.addCriteria(Criteria.where("customerId").is(cart.getCustomerId()));
+		Update update = new Update();
+		update.addToSet("item", cart.getCartItems()); //not the solution
+		mongoTemplate.upsert(query, update, "cart");
 	}
 }
--- src/main/java/com/ibm/ojt/CartRepository.java
+++ src/main/java/com/ibm/ojt/CartRepository.java
@@ -3,6 +3,7 @@ package com.ibm.ojt;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -10,4 +11,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface CartRepository extends MongoRepository<Cart, String>{
 	
 	List<Cart> findByCustomerId(@Param("custid") String customerId);
+	
+	@Query("{'customerId' : ?0, 'cartItems' : {'prodCode' : ?1}}")
+	List<Cart> findAndModifyCartItems(@Param("custid") String customerId, @Param("item") String prodCode);
 }
