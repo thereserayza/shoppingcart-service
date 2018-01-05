c5daad4c620304d6a067384b9bae6e08070b03c1
thereserayza <thereserayzaaaaaa@gmail.com>
2018-01-05 17:58:49 +0800

    addToCart & deleteCartItem working

--- src/main/java/com/ibm/ojt/CartController.java
+++ src/main/java/com/ibm/ojt/CartController.java
@@ -60,18 +60,37 @@ public class CartController {
 	}
 	
 	//adds a new item to cart or updates the quantity/size of item
-	//Problem_Area: How to and/or-operator the update logic
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "/{customerId}")
+	//Problem_Area: What if same item but different sizes???
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "/{customerId}/addupdate")
 	public void addToCart(@RequestBody CartItem cartItem, @PathVariable String customerId) {
 		Criteria custCriteria = Criteria.where("customerId").is(customerId);
-		Criteria prodCriteria = Criteria.where("prodCode").exists(true);
-		Query query = new Query(new Criteria().andOperator(custCriteria, prodCriteria));
-		mongoTemplate.findOne(query, Cart.class, "cart");
-//		Query query = new Query();
-//		query.addCriteria(Criteria.where("customerId").is(customerId).and("prodCode").exists(true));
+		Query query = new Query(new Criteria().andOperator(custCriteria, Criteria.where("cartItems.prodCode").nin(cartItem.getProdCode())));
+		Cart _cart = mongoTemplate.findOne(query, Cart.class, "cart");
+		System.out.println(_cart);
 		Update update = new Update();
 		update.addToSet("cartItems", cartItem);
+		if (_cart != null) { // if new item is not in the cart
+			System.out.println("CART != NULL");
+		}
+		else {
+			System.out.println("CART == NULL");
+			query = new Query(new Criteria().andOperator(custCriteria, Criteria.where("cartItems.prodCode").in(cartItem.getProdCode())));
+		}
 		System.out.println(mongoTemplate.updateFirst(query, update, "cart"));
+	}
+	
+	//deletes item
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, value="/{customerId}/delete")
+	public void deleteCartItem(@RequestBody CartItem cartItem, @PathVariable String customerId) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("customerId").is(customerId).and("cartItems.prodCode").in(cartItem.getProdCode()));
+		Cart _cart = mongoTemplate.findOne(query, Cart.class, "cart");
+		Update update = new Update();
+		if (_cart != null) { // if new item is not in the cart
+			System.out.println("DELETE ITEM FROM CART -");
+			update.pull("cartItems", cartItem);
+			System.out.println(mongoTemplate.updateFirst(query, update, "cart"));
+		}
 	}
 	
 	//updates status of cart (e.g. When customer checks out cart and finishes the process, cart is closed for updates
@@ -81,7 +100,7 @@ public class CartController {
 //		Query query = new Query();
 //		query.addCriteria(Criteria.where("customerId").is(customerId));
 //		Update update = new Update();
-//		update.addToSet("status", "");
+//		update.set("status", "");
 //		mongoTemplate.updateFirst(query, update, "cart");
 //	}
 }
