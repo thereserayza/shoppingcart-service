2b1e6f86b0cb3f651ef20dd398cb655a8af8777e
thereserayza <thereserayzaaaaaa@gmail.com>
2018-01-07 17:17:21 +0800

    Added emptyCart and updateCartStatus logic

--- src/main/java/com/ibm/ojt/CartController.java
+++ src/main/java/com/ibm/ojt/CartController.java
@@ -30,12 +30,8 @@ public class CartController {
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void createCart(@RequestBody Cart cart) {
 		Cart _cart = cartRepository.findOne(cart.getCustomerId());
-		if (_cart != null) {
-			System.out.println("CUSTOMER ID EXISTS");
-		}
-		else {
+		if (_cart == null) {
 			cartRepository.save(cart);
-			System.out.println("CUSTOMER ID DOES NOT EXIST");
 		}
 	}
 	
@@ -47,15 +43,13 @@ public class CartController {
 	@DeleteMapping("/{customerId}")
 	public void deleteCart(@PathVariable String customerId) {
 		Query query = new Query(Criteria.where("customerId").is(customerId));
-		System.out.println(mongoTemplate.findAllAndRemove(query, "cart"));
+		mongoTemplate.findAllAndRemove(query, "cart");
 	}
 
 	@GetMapping("/{customerId}")
 	public Cart findByCustomerId(@PathVariable String customerId) {
-		Query query = new Query();
-		query.addCriteria(Criteria.where("customerId").is(customerId));
+		Query query = new Query().addCriteria(Criteria.where("customerId").is(customerId));
 		Cart _cart = mongoTemplate.findOne(query, Cart.class, "cart");
-		System.out.println("FINDBYCUSTOMERID\n" + _cart);
 		if (_cart == null) {
 			_cart = new Cart();
 			_cart.setCustomerId(customerId);
@@ -73,41 +67,40 @@ public class CartController {
 		Criteria custCriteria = Criteria.where("customerId").is(customerId);
 		Query query = new Query(new Criteria().andOperator(custCriteria, Criteria.where("cartItems.prodCode").nin(cartItem.getProdCode())));
 		Cart _cart = mongoTemplate.findOne(query, Cart.class, "cart");
-		System.out.println(_cart);
-		Update update = new Update();
-		update.addToSet("cartItems", cartItem);
+		Update update = new Update().addToSet("cartItems", cartItem);
 		if (_cart == null) { // if new item is not in the cart
-			System.out.println("CART == NULL");
 			query = new Query(new Criteria().andOperator(custCriteria, Criteria.where("cartItems.prodCode").in(cartItem.getProdCode())));
 		}
-		else {
-			System.out.println("CART != NULL");
-		}
-		System.out.println(mongoTemplate.updateFirst(query, update, "cart"));
+		mongoTemplate.updateFirst(query, update, "cart");
 	}
 	
-	//deletes item
+	//deletes item from cart
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, value="/{customerId}/delete")
 	public void deleteCartItem(@RequestBody CartItem cartItem, @PathVariable String customerId) {
-		Query query = new Query();
-		query.addCriteria(Criteria.where("customerId").is(customerId).and("cartItems.prodCode").in(cartItem.getProdCode()));
+		Query query = new Query().addCriteria(Criteria.where("customerId").is(customerId).and("cartItems.prodCode").in(cartItem.getProdCode()));
 		Cart _cart = mongoTemplate.findOne(query, Cart.class, "cart");
-		Update update = new Update();
 		if (_cart != null) { // if new item is not in the cart
-			System.out.println("DELETE ITEM FROM CART -");
-			update.pull("cartItems", cartItem);
-			System.out.println(mongoTemplate.updateFirst(query, update, "cart"));
+			Update update = new Update().pull("cartItems", cartItem);
+			mongoTemplate.updateFirst(query, update, "cart");
 		}
 	}
 	
-	//updates status of cart (e.g. When customer checks out cart and finishes the process, cart is closed for updates
-	//		until customer adds a new item to cart
-//	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "/{customerId}")
-//	public void updateCartStatus(@PathVariable String customerId) {
-//		Query query = new Query();
-//		query.addCriteria(Criteria.where("customerId").is(customerId));
-//		Update update = new Update();
-//		update.set("status", "");
-//		mongoTemplate.updateFirst(query, update, "cart");
-//	}
+	//deletes item from cart
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, value="/{customerId}/delete/all")
+	public void emptyCart(@PathVariable String customerId) {
+		Query query = new Query().addCriteria(Criteria.where("customerId").is(customerId));
+		Cart _cart = mongoTemplate.findOne(query, Cart.class, "cart");
+		_cart.getCartItems().clear();
+		Update update = new Update().set("cartItems", _cart.getCartItems());
+		mongoTemplate.updateFirst(query, update, "cart");
+	}
+	
+//	updates status of cart (e.g. When customer checks out cart and finishes the process, cart is closed for updates
+//			until customer adds a new item to cart
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, value = "/{customerId}/checkout")
+	public void updateCartStatus(@PathVariable String customerId) {
+		Query query = new Query().addCriteria(Criteria.where("customerId").is(customerId));
+		Update update = new Update().set("status", "CL");
+		mongoTemplate.updateFirst(query, update, "cart");
+	}
 }
