60f88ecaed3a803e94f468c3aa7e184c4b394d59
thereserayza <thereserayzaaaaaa@gmail.com>
2018-01-25 18:57:07 +0800

    Removed exposedIds in RepositoryConfig

--- src/main/java/com/ibm/ojt/ControllerConfig.java
+++ src/main/java/com/ibm/ojt/ControllerConfig.java
@@ -1,16 +1,10 @@
 package com.ibm.ojt;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class ControllerConfig  extends RepositoryRestConfigurerAdapter{
-
-	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-		config.exposeIdsFor(Cart.class);
-	}
+public class ControllerConfig {
 	
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
